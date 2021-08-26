e5a5a29e10fb737fba1662ed7c12a4fdf2cea05c
groubaud101 <guillaume.roubaud1@gmail.com>
2021-08-26 12:38:21 +0200

    Version fonctionnelle avec mon algo (conversion binaire, pas de decalage de bit);TO DO Norm

--- ft_client.c
+++ ft_client.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 13:00:00 by user42            #+#    #+#             */
-/*   Updated: 2021/08/25 22:19:23 by user42           ###   ########.fr       */
+/*   Updated: 2021/08/26 12:37:06 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,38 +50,37 @@ int	ft_send(char *s, int id)
 {
 	static pid_t	pid;
 	static int		c;
-	static int		nb = 128;
+	static int		nb = 256;
 	static char		*str = 0;
-	//int				sig_to_send;
+	int				sig_to_send;
 
-	ft_printf("c : %i, nb : %i, sig : ", c, nb);
+	//ft_printf("DEBUT c : %i, nb : %i\n", c, nb);
 	if (!str)
 	{
 		c = *s;
+		if (!c)
+			exit(0);
 		str = s;
 	}
 	if (id != -1)
 		pid = id;
+	//ft_printf("APRES INIT c : %i, nb : %i\n", c, nb);
 	if (nb == 0)
 	{
-		c = *(str++);
+		str++;
+		c = *str;
+		ft_printf("%c", c);
 		if (!c)
 			exit(0);
-		nb = 128;
+		nb = 256;
 	}
-	//sig_to_send = (c >= nb) * SIGUSR1 + !(c >= nb) * SIGUSR2;
-	if (c >= nb)
-	{
-		kill(pid, SIGUSR1);
-		ft_printf("10\n\n");
-	}
-	else
-	{
-		kill(pid, SIGUSR2);
-		ft_printf("12\n\n");
-	}
+	//ft_printf("APRES nb==0 > c : %i, nb : %i\n", c, nb);
+	sig_to_send = (c >= nb) * SIGUSR1 + !(c >= nb) * SIGUSR2;
+	//ft_printf("sig : %i\n", sig_to_send);
 	c -= (nb * (c >= nb));
 	nb /= 2;
+	kill(pid, sig_to_send);
+	//ft_printf("FIN c : %i, nb : %i\n\n", c, nb);
 	return (1);
 }
 #endif
@@ -95,6 +94,7 @@ void	signal_handler(int sig, siginfo_t *info, void *ucontext)
 			ft_printf("Error kill\n");
 			exit(0);
 		}
+	//ft_printf("retour signal\n");
 }
 
 int	main(int ac, char **av)
@@ -113,8 +113,10 @@ int	main(int ac, char **av)
 	if (sigaction(SIGUSR1, &s, 0) == -1)
 		return (ft_printf("Error sigaction\n"));
 
+	//ft_printf("INIT signal\n");
 	if (ft_send(av[2], pid_server) == -1)
 		return (ft_printf("Error kill\n"));
+	
 	while (1)
 		pause();
 
--- ft_server.c
+++ ft_server.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 13:36:31 by user42            #+#    #+#             */
-/*   Updated: 2021/08/25 22:32:45 by user42           ###   ########.fr       */
+/*   Updated: 2021/08/26 12:36:44 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,17 +46,19 @@ void	signal_handler(int sig, siginfo_t *info, void *ucontext)
 	static int	nb = 256;
 
 	(void)ucontext;
-	ft_printf("c : %i, nb : %i, sig : %i\n", c, nb, sig);
+	//ft_printf("DEBUT c : %i, nb : %i, sig : %i\n", c, nb, sig);
 	if (sig == SIGUSR1)
 		c += nb;
-	if (nb == 0)
+	if (nb == 1)
 	{
 		write(1, &c, 1);
-		nb = 256;
+		nb = 512;
 		c = 0;
 	}
-	kill(info->si_pid, SIGUSR1);
+	//ft_printf("AFTER nb==0 > c : %i, nb : %i, sig : %i\n", c, nb, sig);
 	nb /= 2;
+	kill(info->si_pid, SIGUSR1);
+	//ft_printf("FIN c : %i, nb : %i, sig : %i\n\n", c, nb, sig);
 }
 #endif
 
